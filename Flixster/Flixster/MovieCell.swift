61a2895772e783811377a04582a1747299730acd
Christopher Anastasis <c31anastasis@icloud.com>
2023-09-08 17:23:09 -0400

    Set up table view and custom 'Movie' cell class

---
+++ Flixster/Flixster/MovieCell.swift
@@ -0,0 +1,37 @@
+//
+//  MovieCell.swift
+//  Flixster
+//
+//  Created by Christopher Anastasis on 9/8/23.
+//
+
+import UIKit
+import Nuke
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var movieNameLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    /// Configures the cell's UI for the given track.
+    func configure(with movie: Movie) {
+        movieNameLabel.text = movie.title
+        overviewLabel.text = movie.overview
+
+        // Load image async via Nuke library image loading helper method
+        Nuke.loadImage(with: movie.poster_path, into: movieImageView)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
